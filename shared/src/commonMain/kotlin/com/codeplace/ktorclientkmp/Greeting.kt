37ba5330ed8f68e819c1bd100e7b6c726e1718dc
Leonardocvaleriano <leonardoconsanivaleriano@gmail.com>
2024-11-03 01:11:23 +0000

    Creating a cross-platform mobile application: android implemented

--- shared/src/commonMain/kotlin/com/codeplace/ktorclientkmp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/codeplace/ktorclientkmp/Greeting.kt
@@ -1,9 +1,15 @@
 package com.codeplace.ktorclientkmp
 
-class Greeting {
-    private val platform: Platform = getPlatform()
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
+class Greeting {
+    private val client = HttpClient()
+
+    suspend fun greeting(): String {
+        val response = client.get("https://ktor.io/")
+        return response.bodyAsText()
     }
+
 }
